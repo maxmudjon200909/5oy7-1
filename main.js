46b9b059833f0281a0a46b9c9aab213298278dc9
maxmudjon200909 <mahmudjon200909@gmail.com>
2025-10-04 14:08:17 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let numbers = [2, 4, 9, 10, 45, 67, 8, 90];
+let evenNumbers = [];
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 === 0) {
+    evenNumbers.push(numbers[i]);
+  }
+}
+
+console.log(evenNumbers); // [2, 4, 10, 8, 90]  
+
+
+
+
